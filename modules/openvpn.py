8646d41e0e017ec73c0a9a9a3800a6399b38cac2
furares <vjstc614@gmail.com>
2025-05-16 21:31:04 +0300

    Create openvpn.py

---
+++ modules/openvpn.py
@@ -0,0 +1,22 @@
+import subprocess as s
+from time import sleep
+import os
+from termcolor import colored
+
+class vpn:
+
+    @staticmethod
+    def get_input(prompt, err_msg="[-] Invalid Input"):
+        try:
+            return input(prompt)
+        except (ValueError, KeyboardInterrupt, FileExistsError):
+            print(colored(f"\n{err_msg}", "red", attrs=['bold']))
+            return None 
+    
+    @staticmethod
+    def start():
+        file_path = vpn.get_input("OpenVpn File Path : ")
+        if file_path is None:
+            return None
+        else:
+            s.run(['sudo', 'openvpn', file_path])
